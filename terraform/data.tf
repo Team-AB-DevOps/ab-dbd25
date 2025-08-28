85357f636fb9c0af14df40db06e6f4774a20c9f2
Brian Blume <123966804+Forkeh@users.noreply.github.com>
2025-08-28 21:51:47 +0200

    feat: Terraform basics (#8)
    * feat: terraform basics setup
    
    * chore: husky reformatting dependabot file
    
    * added Ali's pc ssh key variable
    
    * fix: changed port number to Postgres default instead of MySQL
    
    * fix: removed 9100 as an open port for now

---
+++ terraform/data.tf
@@ -0,0 +1,11 @@
+data "digitalocean_ssh_key" "ali_laptop" {
+  name = "ali_laptop"
+}
+
+data "digitalocean_ssh_key" "ali_pc" {
+  name = "ali_pc"
+}
+
+data "digitalocean_ssh_key" "brian_laptop" {
+  name = "brian_laptop"
+}
