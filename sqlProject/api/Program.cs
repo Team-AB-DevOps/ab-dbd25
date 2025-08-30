ed9ed4cc7fbe3f859c898c7dbb1cc354adedc224
Ali Mohammad <alihmohammad@hotmail.com>
2025-08-30 15:21:02 +0200

    feat: established connection to postgres db from api

--- sqlProject/api/Program.cs
+++ sqlProject/api/Program.cs
@@ -1,10 +1,42 @@
+using api.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (File.Exists(".env"))
+{
+    Env.Load(".env");
+}
+
+builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+if (builder.Environment.EnvironmentName == "Test")
+{
+    //TODO: In-memory db for integration tests
+    // builder.Services.AddDbContext<DataContext>(options => options.UseSqlite("DataSource=:memory:"));
+}
+else
+{
+    // PostgreSQL
+    var connectionString = builder.Configuration.GetValue<string>("ConnectionString");
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        throw new InvalidOperationException("ConnectionString not found");
+    }
+
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseNpgsql(connectionString).EnableDetailedErrors();
+    });
+}
 
 var app = builder.Build();
 
