3798250bf63f239a94829a3fbd287d0dd7ca9431
Ali Mohammad <alihmohammad@hotmail.com>
2025-11-12 22:13:49 +0100

    chore: fmt

--- sqlProject/api/Program.cs
+++ sqlProject/api/Program.cs
@@ -67,15 +67,33 @@ builder
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-if (builder.Environment.EnvironmentName == "Test")
-{
-    //TODO: In-memory db for integration tests
-    // builder.Services.AddDbContext<DataContext>(options => options.UseSqlite("DataSource=:memory:"));
-}
-else
-{
+
     // PostgreSQL - Build connection string from environment variables
     var connectionString = builder.Configuration.GetValue<string>("ConnectionString");
+
+    // If no hardcoded connection string, build from environment variables
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        var host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+        var port = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432";
+        var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        var username = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var password = Environment.GetEnvironmentVariable("POSTGRES_PW");
+
+        if (
+            string.IsNullOrEmpty(database)
+            || string.IsNullOrEmpty(username)
+            || string.IsNullOrEmpty(password)
+        )
+        {
+            throw new InvalidOperationException(
+                "Missing required PostgreSQL environment variables: POSTGRES_DB, POSTGRES_USER, POSTGRES_PW"
+            );
+        }
+
+        connectionString =
+            $"server={host};port={port};database={database};userid={username};password={password}";
+    }
 
     // Make connection string available to other services (e.g., DatabaseInitializer)
     builder.Configuration["ConnectionString"] = connectionString;
@@ -87,7 +105,7 @@ else
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableDetailedErrors();
     });
-}
+
 
 // MongoDB: Build from individual env vars or use fallback values
 var mongoHost = Environment.GetEnvironmentVariable("MONGO_HOST") ?? "localhost";
